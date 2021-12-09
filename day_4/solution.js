f01f68cb13c144b083afb55fafd117f489ee5ac7
miki.michaeli <miki.michaeli@appsflyer.com>
2021-12-09 13:07:47 +0200

    solve day 4

--- day_4/solution.js
+++ day_4/solution.js
@@ -1,11 +1,15 @@
 const {
     readLines,
     strToNumber,
-    isEmptyString,
+    isEmpty,
     arraySum
 } = require('../utils');
 
-// returns an array of boards in which each board is a 5x5 grid
+/**
+ * returns an object of shape { boards, draw }
+ * boards - an array of objects of shape {grid, won} where grid is a 5x5 matrix of numbers and won is boolean
+ * draw - an array of numbers
+ */
 const readInputFile = path => {
     const lines = readLines(path);
     const draw = lines[0].split(',').map(strToNumber);
@@ -14,7 +18,7 @@ const readInputFile = path => {
         .filter(s => s.length > 0)
         .map(s => s.trim()
             .split(' ')
-            .filter(isEmptyString)
+            .filter(a => !isEmpty(a))
             .map(strToNumber));
 
     const boards = [];
@@ -24,40 +28,57 @@ const readInputFile = path => {
     return { boards, draw };
 }
 
-function findAndMarkReturningLocation(board, num) {
-    for (let row = 0; row < board.length; row++) {
-        for (let column = 0; column < board[0].length; column++) {
-            if (board[row][column] === num) {
-                board[row][column] = null;
-                return { row, column };
+const isWinningArray = (a) => isEmpty(a.filter(n => n !== null));
+const hasWinningRow = (board) => board.map(isWinningArray).find(Boolean) !== undefined;
+const hasWinningColumn = (board) => {
+    for (let column = 0; column < board[0].length; column++) {
+        const col = board.map(row => row[column]);
+        if (isWinningArray(col)) {
+            return true;
+        }
+    }
+    return false;
+}
+const makeMove = (board, drawnNumber) => board.map(row => row.map(n => n == drawnNumber ? null : n));
+const calculateBoardScore = ({board, lastNum}) =>
+    arraySum(board.map(arraySum)) * lastNum;
+
+// First puzzle of the day
+const findFirstWinningBoard = ({ boards, draw }) => {
+    for (let n of draw) {
+        for (let i = 0; i < boards.length; i++) {
+            boards[i] = makeMove(boards[i], n);
+            if (hasWinningRow(boards[i]) || hasWinningColumn(boards[i])) {
+                return { board: boards[i], lastNum: n };
             }
         }
     }
     return null;
 }
 
-function isWinningLocation(board, { row, column }) {
-    const isRowMarked = board[row].filter(Boolean).length === 0;
-    if (isRowMarked) return true;
-    for (let i = 0; i < board.length; i++) {
-        if (board[i][column]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-const calculateBoardScore = (board, lastDrawnNumber) => arraySum(board.map(arraySum)) * lastDrawnNumber;
-
-function findFirstWinningBoard({ boards, draw }) {
+// Second puzzle of the day
+const findLastWinningBoard = ({ boards, draw }) => {
+    let lastWinner = null;
+    let lastWinningNumber = null;
     for (let n of draw) {
-        for (let b of boards) {
-            const location = findAndMarkReturningLocation(b, n);
-            if (location && isWinningLocation(b, location)) {
-                return calculateBoardScore(b, n);
+        for (let i = 0; i < boards.length; i++) {
+            if (boards[i].won) continue;
+            boards[i] = makeMove(boards[i], n);
+            if (hasWinningRow(boards[i]) || hasWinningColumn(boards[i])) {
+                boards[i].won = true;
+                lastWinner = boards[i];
+                lastWinningNumber = n;
             }
         }
     }
+
+    return { board: lastWinner, lastNum: lastWinningNumber };
 }
 
-console.log(findFirstWinningBoard(readInputFile('input.txt')));
+const inputFileName = process.argv[2];
+const inputFilePath = `${__dirname}/${inputFileName}`;
+const input = readInputFile(inputFilePath);
+const firstWinningBoard = findFirstWinningBoard(input);
+const lastWinningBoard = findLastWinningBoard(input);
+console.log("first winning board score is", calculateBoardScore(firstWinningBoard));
+console.log("last winning board score is", calculateBoardScore(lastWinningBoard));
--- utils.js
+++ utils.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 const readLines = (filePath) => fs.readFileSync(filePath, { encoding: 'utf-8', flag: 'r' }).split('\n');
 const strToNumber = s => Number(s.trim());
-const isEmptyString = (s) => s.length > 0;
+const isEmpty = (s) => s.length === 0;
 const arraySum = (array) => array.reduce((a, b) => (a || 0) + (b || 0), 0);
 
 module.exports = {
     readLines,
     strToNumber,
-    isEmptyString,
+    isEmpty,
     arraySum
 };
